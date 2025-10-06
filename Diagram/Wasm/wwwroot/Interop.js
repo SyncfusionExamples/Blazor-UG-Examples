b5d499717aeead6359ce1ffa3bd161afac273843
Ramya <ramya.subramani@syncfusion.com>
2025-10-06 11:18:44 +0530

    Updated Project folder for Diagram component

---
+++ Diagram/Wasm/wwwroot/Interop.js
@@ -0,0 +1,27 @@
+﻿function getDiagramFileName() {
+    return document.getElementById('diagramName').innerHTML.toString();
+}
+
+function saveDiagram(data, filename) {
+    if (window.navigator.msSaveBlob) {
+        let blob = new Blob([data], { type: 'data:text/json;charset=utf-8,' });
+        window.navigator.msSaveOrOpenBlob(blob, filename + '.json');
+    } else {
+        let dataStr = 'data:text/json;charset=utf-8,' + encodeURIComponent(data);
+        let a = document.createElement('a');
+        a.href = dataStr;
+        a.download = filename + '.json';
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+    }
+}
+
+function click() {
+    document.getElementById('UploadFiles').click();
+}
+function loadFile(file) {
+    var base64 = file.rawFile.replace("data:application/json;base64,", "");
+    var json = atob(base64)
+    return json;
+}
