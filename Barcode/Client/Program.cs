01e30cc3c5eb472f91c741583b2d1da4555fa5c6
Ramya <ramya.subramani@syncfusion.com>
2025-10-09 11:31:50 +0530

    984376: Updated barcode example

--- Barcode/Client/Program.cs
+++ Barcode/Client/Program.cs
@@ -8,5 +8,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
+builder.Services.AddSyncfusionBlazor(options => { });
 await builder.Build().RunAsync();
