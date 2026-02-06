5c8cac565e0567271f9a6959f766e812eaaab474
DeebigaMurugan <deebiga.murugan@syncfusion.com>
2026-02-06 22:14:03 +0530

    1007690: Add the Aggregation and its related topics in Blazor pivotTable.

---
+++ PivotTable/NET8/WasmApp/WasmApp.Client/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+//Register Syncfusion license
+//Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("YOUR LICENSE KEY");
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddSyncfusionBlazor();	
+
+await builder.Build().RunAsync();
